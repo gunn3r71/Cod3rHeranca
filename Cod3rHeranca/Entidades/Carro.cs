5d3377d0b1c45c403aa168c7f840d9d530ccb64e
gunn3r71 <lucasgunner3@gmail.com>
2021-05-09 12:05:39 -0300

    Adicionar arquivos de projeto.

---
+++ Cod3rHeranca/Entidades/Carro.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cod3rHeranca.Entidades
+{
+    class Carro
+    {
+    }
+}
---
+++ Cod3rHeranca/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cod3rHeranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
